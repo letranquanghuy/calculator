510dd73b52ab6b7b6044a85f510e44bb09e375f0
eru1hc <huy.letranquang@vn.bosch.com>
2025-01-03 17:46:12 +0700

    refactor and format display

--- calculator/views.py
+++ calculator/views.py
@@ -6,40 +6,70 @@ from django.views.decorators.http import require_POST
 import math
 from math import sqrt, factorial, pi
 import sys
+from sympy import sympify, oo, zoo, I
+from decimal import Decimal
 
 from calculator.models import CalculationHistory
-# sys.set_int_max_str_digits(0)
+
+
+sys.set_int_max_str_digits(0)
+
+
 # Create your views here.
 def index(request):
     history = CalculationHistory.objects.all()
-    return render(request, r"calculator\index.html", {'history': history})
+    formatted_history = [
+        {
+            "expression": format_display_expression(record.expression),
+            "result": format_display_number(record.result),
+            "created_at": record.created_at,
+        }
+        for record in history
+    ]
+    return render(request, r"calculator\index.html", {"history": formatted_history})
+
+
+def format_display_expression(value):
+    value = value.replace("+-", "-")
+    return re.sub(
+        r"\d+(\.\d+)?", lambda match: format_display_number(match.group()), value
+    )
+
+
+def format_display_number(number_str):
+    print("raw number_str", number_str)
+    number = Decimal(number_str)
+    number_str = f"{number:.10f}".rstrip("0").rstrip(".")
+    print("number_str", number_str)
+    if "." in number_str:
+        integer_part, fractional_part = number_str.split(".")
+        formatted_integer = f"{int(integer_part):,}"
+        print("fractional_part", fractional_part)
+        return f"{formatted_integer}.{fractional_part}"
+    else:
+        return f"{int(number_str):,}"
 
 
 def preprocess_expression(expression):
     """Preprocess a mathematical expression string for evaluation."""
-    open_count = expression.count('(')
-    close_count = expression.count(')')
+    print("expression in preprocess", expression)
+    open_count = expression.count("(")
+    close_count = expression.count(")")
 
     # If '(' are greater than ')', append ')' to balance
     if open_count > close_count:
-        expression += ')' * (open_count - close_count)
+        expression += ")" * (open_count - close_count)
     elif open_count < close_count:
         return "ERROR"
-    
+
     expression = apply_basic_transformations(expression)
     expression = normalize_number(expression)
-    expression = replace_scientific_notation(expression)
-    expression = process_factorial(expression)
     expression = process_pi(expression)
     expression = process_parenthesis(expression)
     if not validate_balance_parenthese(expression):
         return "ERROR"
     return expression
 
-
-def replace_scientific_notation(expression):
-    # Match scientific notation in the form of e-n where n is a number
-    return re.sub(r'e-(\d+)', r'*10**(-\1)', expression)
 
 def validate_balance_parenthese(expression):
     stack = []
@@ -52,21 +82,14 @@ def validate_balance_parenthese(expression):
             stack.pop()
     return not stack
 
+
 def apply_basic_transformations(expression):
     """Apply basic symbol transformations for mathematical expressions."""
-    transformations = [
-        ("x", "*"),
-        ("÷", "/"),
-        ("√", "sqrt"),
-        ("^", "**")
-    ]
+    transformations = [("x", "*"), ("÷", "/"), ("√", "sqrt"), ("^", "**")]
     for old, new in transformations:
         expression = expression.replace(old, new)
     return expression
 
-def process_factorial(expression):
-    """Replace factorial notation with factorial."""
-    return re.sub(r"(\d+)!", r"(factorial(\1))", expression)
 
 def process_parenthesis(expression):
     """Ensure multiplication is explicitly added around parentheses where necessary."""
@@ -81,18 +104,21 @@ def process_parenthesis(expression):
         expression = re.sub(pattern, replacement, expression)
     return expression
 
+
 def process_pi(expression):
     """Replace pi symbol with pi."""
     return expression.replace("π", f"({pi})")
 
+
 def normalize_number(expression):
     def normalize_match(match):
-        if '.' not in match.group():
+        if "." not in match.group():
             return str(int(match.group()))
-        return str(float(match.group()))
+        return f"{float(match.group()):.10f}".rstrip("0").rstrip(".")
 
-    normalized_expression = re.sub(r'(\d+(\.\d+)?e[+-]?\d+)', lambda match: f"({match.group()})", expression)
-    normalized_expression = re.sub(r'(?<!\w)(0+)?\d+(\.\d+)?(?!\w)', normalize_match, normalized_expression)
+    normalized_expression = re.sub(
+        r"(?<!\w)(0+)?\d+(\.\d+)?(?!\w)", normalize_match, expression
+    )
     return normalized_expression
 
 
@@ -103,70 +129,111 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def evaluate_expression(expression):
-    result = eval(expression)
-    if isinstance(result, float):
-        result = round(result, 10)
-    if result.is_integer():
-        result = int(result)
+    result = sympify(expression).evalf()
+    if result is zoo or result is oo:
+        raise ZeroDivisionError
+
+    if result.has(I):
+        raise ValueError(f"Complex number detected in expression: {expression}")
+
+    if result.is_real:
+        # Làm tròn đến 10 chữ số thập phân
+        result = round(float(result), 10)
+        if result.is_integer():
+            result = int(result)
     return str(result)
+
 
 def replace_match_with_evaluation(match):
     expression = match.group(0)
-    return evaluate_expression(expression)
+    return f"({evaluate_expression(expression)})"
+
 
 def replace_factorial(match):
     if match.group(1):
-        num = match.group(1)
-        return f"factorial({num})"
+        num_str = match.group(1)
+    else:
+        num_str = match.group(0)[1:-2]
+    num = float(num_str)
+    if not num.is_integer() or num < 0:
+        raise ValueError(
+            f"Factorial is not defined for non-integer values in expression: {num_str}"
+        )
+    return f"factorial({int(num)})"
 
-    if match.group(3):
-        expression = match.group(3)
-        return f"factorial({expression})"
 
 def calculate(expression):
     i = 0
     old_expression = None
     while not is_number(expression):
+        print("expression in calculate", expression)
         if old_expression == expression:
-            expression = "ERROR"
+            expression = evaluate_expression(expression)
             break
         old_expression = expression
         # calculate the expression inside parentheses
-        expression = re.sub(r"(?<!\w)(\(([-+*/\d.\s]+)\))", replace_match_with_evaluation, expression)
+        expression = re.sub(
+            r"(\(([-+]?\d*\.?\d+|\(-?\d+\.?\d*\))([+\-*/^]+[-+\-*/\d.]+)+\))",
+            replace_match_with_evaluation,
+            expression,
+        )
+        print("expression in calculate", expression)
         # calculate the function
-        expression = re.sub(r"\b[a-zA-Z_]+\(([-+*/\d.\s]+)\)", replace_match_with_evaluation, expression)
+        expression = re.sub(
+            r"\b[a-zA-Z_]+\(([-+*/\d.\s]+)\)", replace_match_with_evaluation, expression
+        )
+        print("expression in calculate", expression)
         # process factorial
-        expression = re.sub(r"(\d+(\.\d+)?)!|(\(([-+*/\d.\s]+)\))!", replace_factorial, expression)
-        print(f"after factorial: {expression}")
-        if "(" not in expression and ")" not in expression:
+        expression = re.sub(
+            r"(\d+(\.\d+)?)!|(\((-?\d*\.?\d+)\))!", replace_factorial, expression
+        )
+        print("expression after process factorial", expression)
+        if "sqrt" not in expression and "!" not in expression:
             expression = evaluate_expression(expression)
+        print("expression in calculate", expression)
     else:
         expression = evaluate_expression(expression)
+    print("expression after calculate", expression)
     return expression
+
 
 @require_POST
 def calculate_result(request):
+    record = False
     expression = request.POST["expression"]
+    if not expression:
+        return JsonResponse({"result": ""}, status=200)
+
     original_expression = expression
-    print("pre_expression", expression)
     expression = preprocess_expression(expression)
     try:
         print("expression", expression)
         result = calculate(expression)
-        history_entry = CalculationHistory(expression=original_expression, result=str(result))
-        history_entry.save()
-    except ZeroDivisionError and OverflowError:
-        result = "INFINITY"
+        display_result = format_display_number(result)
+        # Get the last entry from the database
+        last_entry = CalculationHistory.objects.all().last()
+        if not last_entry or (
+            last_entry.expression != original_expression or last_entry.result != result
+        ):
+            history_entry = CalculationHistory(
+                expression=original_expression, result=result
+            )
+            history_entry.save()
+            record = True
+    except ZeroDivisionError or OverflowError:
+        display_result = "INFINITY"
     except ValueError as e:
         if "Exceeds the limit" in str(e):
-            result = "INFINITY"
+            display_result = "INFINITY"
         else:
-            result = "ERROR"
+            display_result = "ERROR"
     except Exception:
-        result = "ERROR"
+        display_result = "ERROR"
 
-    return JsonResponse({'result': str(result)}, status=200)
+    return JsonResponse({"result": display_result, "record": record}, status=200)
+
 
 @require_POST
 def handle_percentage(request):
@@ -175,17 +242,26 @@ def handle_percentage(request):
     last_index = 0
     first_open = None
     stack = []
-
+    if not expression:
+        return JsonResponse({"result": ""}, status=200)
     if expression[-1] != ")":
-        result = re.sub(
-            r'(\d+(\.\d+)?(e[+-]?\d+)?)%', 
-            lambda match: calculate(preprocess_expression(match.group(1)+"/100")), 
-            expression + "%"
-        )
-        if "%" in result:
-            result = expression
-        if result == "π":
-            result = pi/100
+        print("its num", expression)
+        if expression[-1] == "π":
+            result = expression[:-1] + str(pi/100)
+        else:
+            result = re.sub(
+                r"([0-9.]+)%",
+                lambda match: f"{float(calculate(preprocess_expression(match.group(1) + "/100"))):.10f}".rstrip(
+                    "0"
+                ).rstrip(
+                    "."
+                ),
+                expression + "%",
+            )
+            print("result after %", result)
+            if "%" in result:
+                result = expression
+
     else:
         for i in range(len(expression) - 1, -1, -1):
             last_index = i
@@ -193,11 +269,11 @@ def handle_percentage(request):
                 stack.append(expression[i])
             elif expression[i] == "(":
                 stack.pop()
-                if first_open is None:
+                if first_open is None and not (i != 0 and expression[i - 1] == "√"):
                     first_open = i
             sub_expression = expression[i] + sub_expression
             if not stack:
-                if expression[i-1] == "√":
+                if expression[i - 1] == "√":
                     sub_expression = "√" + sub_expression
                     last_index = i - 1
                 break
@@ -205,15 +281,20 @@ def handle_percentage(request):
             result = expression[:first_open] + "ERROR"
         else:
             print(sub_expression)
-            result =expression[:last_index] + calculate(preprocess_expression(sub_expression)+'/100')
+            result = expression[:last_index] + calculate(
+                preprocess_expression(sub_expression) + "/100"
+            )
 
-    return JsonResponse({'result': str(result)}, status=200)
+    display_result = format_display_expression(str(result))
+    print("display_result", display_result)
+    return JsonResponse({"result": display_result}, status=200)
+
 
 @require_POST
 def clear_history(request):
     try:
         # Assuming the history is stored in a model called History
         CalculationHistory.objects.all().delete()  # Delete all history records
-        return JsonResponse({'status': 'success'}, status=200)
+        return JsonResponse({"status": "success"}, status=200)
     except Exception as e:
-        return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
+        return JsonResponse({"status": "error", "message": str(e)}, status=400)
